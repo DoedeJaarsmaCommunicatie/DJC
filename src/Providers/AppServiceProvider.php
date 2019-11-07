ab38d06c3d6cd263fb1a1b4b3472a32eac5ee2d9
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2019-11-07 14:47:50 +0100

    [feature/lazy-loading] Added a new file size 20x20

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -17,4 +17,21 @@ class AppServiceProvider
 			new $provider();
 		}
 	}
+	
+	public function register(): void
+    {
+        $this->addImageSize();
+    }
+    
+    
+    private function addImageSize(): void
+    {
+        \add_image_size('lazy-thumbnail', 20, 20, false);
+        
+        \add_filter( 'image_size_names_choose', static function ( $sizes ) {
+            return array_merge( $sizes, [
+                'lazy-thumbnail' => __( 'Lazy thumbnail' ),
+            ] );
+        } );
+    }
 }
