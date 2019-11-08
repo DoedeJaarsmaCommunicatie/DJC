4cbde6dbb3b3e276e5da61a850f0d7ac09c71f0f
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2019-11-08 13:08:12 +0100

    Added reminder and reminderOnDesktop function to show reminders (#1)

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,11 +6,14 @@ const common = async () =>
     (await import(/* webpackChunkName: "dist/scripts/routes/common" */'./routes/Common')).default;
 const home = async () =>
     (await import(/* webpackChunkName: "dist/scripts/routes/home" */ './routes/Home')).default;
+const project = async () =>
+    (await import(/* webpackChunkName: "dist/scripts/routes/project" */ './routes/Project')).default
 
 
 const routes = new Router({
     common: common(),
-    home: home()
+    home: home(),
+    singleProject: project()
 });
 
 Ready(() => routes.loadEvents());
